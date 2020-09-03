3efed0d1ff8b27412828b45d79c60824ad3df92e
akjw <aseykoh@gmail.com>
2020-09-03 14:55:35 +0800

    deploy

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
-// require('dotenv').config()
+require('dotenv').config()
 
 const mongoose = require('mongoose')
+
 
 //== connect to mongoose 
 mongoose.connect(
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -3,7 +3,7 @@ const User = require('../models/user.model');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const checkToken = require('../config/config');
-// require('dotenv').config()
+require('dotenv').config()
 
 
 /* 
