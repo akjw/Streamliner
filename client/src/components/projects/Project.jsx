7dd62b458101a931f594d2ec46d3da2e09228f46
akjw <aseykoh@gmail.com>
2020-09-15 14:53:10 +0800

    update accordion styling

--- client/src/components/projects/Project.jsx
+++ client/src/components/projects/Project.jsx
@@ -145,21 +145,16 @@ useEffect(() => {
           <Row className="mt-4">
             <Col>
                  {project.phases.length ?
-                <Accordion defaultActiveKey={project.activePhase ? project.activePhase : project.phases[0]._id}>
-                {project.phases.map((phase, i) => (
-                      <Card key={i}>
-                        <Accordion.Toggle as={Card.Header} eventKey={phase._id} className="accordion-theme">
-                          <div className="d-flex justify-content-start">
-                            <h4>{phase.name}
-                            {(project.activePhase && phase._id.toString() == project.activePhase.toString()) && <Badge pill variant="primary" className="mx-2 purple">Active</Badge>}
-                            </h4>
-                          </div>
-                        </Accordion.Toggle>
-                        <Accordion.Collapse eventKey={phase._id}>
-                          <Card.Body>
-                          <h4>{phase.subheader}</h4>
-                          <div className="d-flex justify-content-end">
-                          <h4> 
+                  <Collapse accordion bordered={false} defaultActiveKey={project.activePhase ? project.activePhase : project.phases[0]._id}>
+                  {project.phases.map(phase => (
+                  <Panel header={
+                  <div className="d-flex justify-content-start">
+                    <p className="panel-header">
+                    {phase.name}
+                    {(project.activePhase && phase._id.toString() == project.activePhase.toString()) && <Badge pill variant="primary" className="mx-2 purple">Active</Badge>}
+                    </p>
+                  </div>} key={phase._id} className="accordion-theme">
+                  <AnCard className="" extra={ <h4> 
                             <Tooltip title={`New ${phase.subheader}`}>
                             <Link to={`/phases/${phase._id}/deliverables/new`}>
                                 <PlusSquareTwoTone className="mx-2"/>
@@ -181,8 +176,8 @@ useEffect(() => {
                           </Popconfirm>
                           </>
                          : ''}
-                         </h4>
-                          </div>
+                         </h4>}>
+                          <h4 className="mb-4">{phase.subheader}</h4>
                                 <Container fluid className="mt-4">
                                 <Row>
                                   {(!isLoading && !phase.deliverables.length) && <p>No {phase.subheader} yet</p>}
@@ -217,11 +212,87 @@ useEffect(() => {
                                   })}
                                 </Row>
                               </Container>
-                          </Card.Body>
-                        </Accordion.Collapse>
-                      </Card>
-                ))}
-              </Accordion> 
+                          </AnCard>
+                  </Panel>
+                   ))}
+                </Collapse>
+              //   <Accordion defaultActiveKey={project.activePhase ? project.activePhase : project.phases[0]._id}>
+              //   {project.phases.map((phase, i) => (
+              //         <Card key={i}>
+              //           <Accordion.Toggle as={Card.Header} eventKey={phase._id} className="accordion-theme">
+              //             <div className="d-flex justify-content-start">
+              //               <h4>{phase.name}
+              //               {(project.activePhase && phase._id.toString() == project.activePhase.toString()) && <Badge pill variant="primary" className="mx-2 purple">Active</Badge>}
+              //               </h4>
+              //             </div>
+              //           </Accordion.Toggle>
+              //           <Accordion.Collapse eventKey={phase._id}>
+              //             <Card.Body>
+              //             <h4>{phase.subheader}</h4>
+              //             <div className="d-flex justify-content-end">
+              //             <h4> 
+              //               <Tooltip title={`New ${phase.subheader}`}>
+              //               <Link to={`/phases/${phase._id}/deliverables/new`}>
+              //                   <PlusSquareTwoTone className="mx-2"/>
+              //               </Link>
+              //             </Tooltip>
+              //             {user ? (project && (user._id.toString() == project.createdBy._id.toString())) &&
+              //             <>
+              //             <Tooltip title="Edit Phase">
+              //               <Link to={`/phases/${phase._id}`} className="mx-2"><EditOutlined  /></Link>
+              //             </Tooltip>
+              //             <Popconfirm
+              //                   title="Are you sure you want to delete this phase?"
+              //                   onConfirm={()=>deletePhase(phase._id)}
+              //                   onCancel={cancel}
+              //                   okText="Yes"
+              //                   cancelText="No"
+              //                 >
+              //                  <DeleteFilled id={phase._id} className="mx-2"/>
+              //             </Popconfirm>
+              //             </>
+              //            : ''}
+              //            </h4>
+              //             </div>
+              //                   <Container fluid className="mt-4">
+              //                   <Row>
+              //                     {(!isLoading && !phase.deliverables.length) && <p>No {phase.subheader} yet</p>}
+              //                     {(!isLoading && phase.deliverables ) && phase.deliverables.map(d => {
+              //                       return <Col key={d._id} md="3">
+              //                         <AnCard className={d.isComplete ? 'complete' : '' }
+              //                         extra={!d.isComplete ? <Tooltip title="Mark as complete">
+              //                         <CheckOutlined onClick={()=>toggleComplete(d._id, markComplete)}/>
+              //                       </Tooltip> : <Tooltip title="Mark as in-progress">
+              //                         <CheckCircleOutlined onClick={()=>toggleComplete(d._id, markInProgress)}/>
+              //                       </Tooltip>}
+              //                         >
+              //                             <b>{d.name}</b>
+              //                             <p>{d.description}</p>
+              //                             <div>
+              //                               <p>Deadline: {moment(d.deadline).format('MMMM Do YYYY')}</p>
+              //                             </div>
+              //                             <div>
+              //                               <p>Status: {d.isComplete? 'Complete' : 'In-Progress'}</p>
+              //                             </div>
+              //                             {(moment(d.deadline).tz('Asia/Singapore').diff(now, 'days') < 0 && !d.isComplete) ? <p className="red"><i>Overdue</i></p>
+              //                             : moment(d.deadline).tz('Asia/Singapore').diff(now, 'days') > 1 ? <p><b>Due in {moment(d.deadline).tz('Asia/Singapore').diff(now, 'days')} days</b></p> 
+              //                             : moment(d.deadline).tz('Asia/Singapore').diff(now, 'days') > 0 ? <p><b>Due in {moment(d.deadline).tz('Asia/Singapore').diff(now, 'days')} day</b></p> 
+              //                             : moment(d.deadline).tz('Asia/Singapore').diff(now, 'days') < -1 ? <p><b>Due {moment(now).tz('Asia/Singapore').diff(d.deadline, 'days')} days ago</b></p> 
+              //                             :  moment(d.deadline).tz('Asia/Singapore').diff(now, 'days') < 0 ? <p><b>Due {moment(now).tz('Asia/Singapore').diff(d.deadline, 'days')} day ago</b></p> 
+              //                             : <p><b>Due today</b></p>}
+              //                               <div>
+              //                                 <Link to={`/deliverables/${d._id}`}>Edit</Link>
+              //                               </div>
+              //                         </AnCard>
+              //                       </Col>
+              //                     })}
+              //                   </Row>
+              //                 </Container>
+              //             </Card.Body>
+              //           </Accordion.Collapse>
+              //         </Card>
+              //   ))}
+              // </Accordion> 
               : <p>No phases to show</p>}
             </Col>
           </Row>
