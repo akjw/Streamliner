aefe5c2de1a189c9f48f63761ec25a6df2056593
akjw <aseykoh@gmail.com>
2020-09-15 15:12:23 +0800

    remove landing image for mobile

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -14,21 +14,20 @@ useEffect(() => {
 }, [])
   return (
     <div className="landing-div">
-      <img src={Wave} className="wave"/>
-      <div className="nav-landing mt-12 mx-4">
-        <div className="d-flex justify-content-end mt-4">
-          <div className="row mt-4">
-            <div className="col mt-4">
-               <p className="landing-header mt-6">We keep things smooth.</p>
-               <p className="landing-text">Manage your projects at every stage — right here.</p>
-               <Link to="/register" className="btn btn-outline-light btn-lg nav-landing">
-                  Sign up
-                </Link>
-               {/* <Button variant="outline-light" size="lg" className="nav-landing">Sign up</Button> */}
+      <div className="wave"></div>
+        <div className="nav-landing mt-12 mx-4">
+          <div className="d-flex justify-content-end mt-4">
+            <div className="row mt-4">
+              <div className="col mt-4">
+                <p className="landing-header mt-6">We keep things smooth.</p>
+                <p className="landing-text">Manage your projects at every stage — right here.</p>
+                <Link to="/register" className="btn btn-outline-light btn-lg nav-landing">
+                    Sign up
+                  </Link>
+                </div>
               </div>
             </div>
-          </div>
-      </div>
+        </div>
     </div>
   )
 }
